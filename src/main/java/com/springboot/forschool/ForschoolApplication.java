da20cf8c6b525f5869e496deb2e967e44c2d09d0
Tayfun Sen <sen.ghost@outlook.com>
2020-11-17 20:02:30 +0100

    Habe  bei den resources die Application.properties hinzugefuegt

---
+++ src/main/java/com/springboot/forschool/ForschoolApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.forschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForschoolApplication.class, args);
+    }
+
+}
